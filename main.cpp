245b660c9b5cb1aaf2525d42781e3d68122f367d
KrystianKiznar <krystian.kiznar@gmail.com>
2022-09-21 12:13:50 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "PetriDish.h"
+#include "Bacillus.h"
+#include "Bacterium.h"
+#include "Coccus.h"
+#include "Spirillum.h"
+#include <windows.h>
+#include <ctime>
+
+int main() {
+	PetriDish talerz(50);
+	talerz.initializeBoard();
+	talerz.displayDish();
+	Sleep(4000);
+
+	for (int i{ 0 }; i < 10; i++) {
+		std::cout << "Turn: "<< i << std::endl;
+		//talerz.howManyBacterial();
+		talerz.isBacteriaAlive();
+		talerz.clearDish();
+		talerz.displayDish();
+
+		talerz.bacteriaPovielations();
+		talerz.frankenstein();
+		Sleep(3000);
+		system("cls");
+	}
+
+	//talerz.displayDish();
+
+	return 0;
+}
